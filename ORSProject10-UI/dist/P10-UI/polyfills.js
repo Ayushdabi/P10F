2bad36ce03201c0b581dc6e92690869eb21ea3f7
Ayushdabi <udaydabi7@gmail.com>
2025-08-14 16:47:31 +0530

    done

--- ORSProject10-UI/dist/P10-UI/polyfills.js
+++ ORSProject10-UI/dist/P10-UI/polyfills.js
@@ -5788,8 +5788,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! E:\Rays Technology\ORSProject10-UI\src\polyfills.ts */"./src/polyfills.ts");
-module.exports = __webpack_require__(/*! E:\Rays Technology\ORSProject10-UI\node_modules\@angular-devkit\build-angular\src\angular-cli-files\models\jit-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js");
+__webpack_require__(/*! C:\Workspace\Project-10\Project10-WorkSpace\ORSProject10-UI\src\polyfills.ts */"./src/polyfills.ts");
+module.exports = __webpack_require__(/*! C:\Workspace\Project-10\Project10-WorkSpace\ORSProject10-UI\node_modules\@angular-devkit\build-angular\src\angular-cli-files\models\jit-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js");
 
 
 /***/ })
--- ORSProject10-UI/dist/P10-UI/styles.js
+++ ORSProject10-UI/dist/P10-UI/styles.js
@@ -557,7 +557,7 @@ if(false) {}
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! E:\Rays Technology\ORSProject10-UI\src\styles.css */"./src/styles.css");
+module.exports = __webpack_require__(/*! C:\Workspace\Project-10\Project10-WorkSpace\ORSProject10-UI\src\styles.css */"./src/styles.css");
 
 
 /***/ })
